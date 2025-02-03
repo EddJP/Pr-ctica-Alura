69859173939d09b117b82c935e370dabac3af922
Eduardo <eduardo.pj.11@comunidad.unam.mx>
2025-02-03 14:17:07 -0600

    Captutar valor de entrada 2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 document.addEventListener("DOMContentLoaded", () => {
    //Caputurar el valor del campo de entrada
     const inputNombre = document.getElementById("nombre");
     const botonAgregar = document.getElementById("agregar");
+
+    botonAgregar.addEventListener("click"), ()  => {
+        //Capturar el valor del campo de entrada
+        let nombre = inputNombre.ariaValueMax.trim(); 
+
+});  
