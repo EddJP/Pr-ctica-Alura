f87ebffad43d363508d2243f4eb3a9bd8893b91e
Eduardo <eduardo.pj.11@comunidad.unam.mx>
2025-02-03 14:51:06 -0600

    Corrección 2

--- app.js
+++ app.js
@@ -4,23 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputNombre = document.getElementById("amigo");
     const botonAgregar = document.querySelector(".button-add");
     const listaAmigos = document.getElementById("listaAmigos");
-    
-    botonAgregar.addEventListener("click", agregarAmigo);
-    
+
     function agregarAmigo() {
         let nombre = inputNombre.value.trim();
-        
+
         if (nombre === "") {
             alert("Por favor, inserte un nombre.");
             return;
         }
-        
+
         amigos.push(nombre);
         inputNombre.value = "";
         actualizarLista();
         console.log(amigos);
     }
-    
+
     function actualizarLista() {
         listaAmigos.innerHTML = "";
         amigos.forEach(amigo => {
@@ -29,4 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
             listaAmigos.appendChild(li);
         });
     }
+
+    // Agregar evento al botón después de que el DOM se haya cargado
+    botonAgregar.addEventListener("click", agregarAmigo);
 });
